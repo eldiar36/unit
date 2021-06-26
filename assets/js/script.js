ac1d0437692a9a41fc6722c1099873d909d800e2
eldiar36 <eldiarrd@gmail.com>
2021-06-26 22:49:07 +0600

    edadadada

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+(function($) {
+    $(function() {
+        $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+            $(this)
+                .addClass("active")
+                .siblings()
+                .removeClass("active")
+                .closest("div.tabs")
+                .find("div.tabs__content")
+                .removeClass("active")
+                .eq($(this).index())
+                .addClass("active");
+        });
+    });
+})(jQuery);
+const accordions = document.querySelectorAll(".accordion-wrapper");
+for (const accordion of accordions) {
+    const panels = accordion.querySelectorAll(".accordion");
+    for (const panel of panels) {
+        const head = panel.querySelector(".accordion-header");
+        head.addEventListener('click', () => {
+            for (const otherPanel of panels) {
+                if (otherPanel !== panel) {
+                    otherPanel.classList.remove('accordion-expanded');
+                }
+            }
+            panel.classList.toggle('accordion-expanded');
+        });
+    }
+}
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+});
+
